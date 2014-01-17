63f27f32150214c0cfb0f650a098337ccbcf54ef
MissMuffin <fyyree@gmail.com>
2014-01-17 10:55:34 +0100

    First commit, clean rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Videogames::Application.config.secret_key_base = '8698b6d5f1da2525fcb64ba27b7eb605dd84f863c0f93f2ed67d7841c7b175fc46541a95549b43866428348cf2d9463677cca7a223f5981aeaba25c3aa6a780c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Videogames::Application.config.session_store :cookie_store, key: '_Videogames_session'
